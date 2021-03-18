65285de345e342cc5cac24c1e6f8ab233b6041cf
paula-jfe <paula.jfe@gmail.com>
2021-03-18 10:55:02 -0300

    Atualizando versao RTL

--- src/test/exercise-one.test.js
+++ src/test/exercise-one.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { render, fireEvent, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
-
-afterEach(cleanup);
 
 // arquivo App.test.js pode servir de exemplo
 describe('Teste da aplicação, testando o botão e sua funcionalidade', () => {
@@ -12,7 +11,7 @@ describe('Teste da aplicação, testando o botão e sua funcionalidade', () => {
   });
 
   test(`Ao clicar no botão, é necessário adicionar o que o usuário digitou à lista`, () => {
-    // Use os fireEvent, para simular a digitação do usuário e o clique.
+    // Use o userEvent, para simular a digitação do usuário e o clique.
     const {getByLabelText,queryByText} = render(<App />);
 
   });
