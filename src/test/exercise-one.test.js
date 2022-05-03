788dcec060a47bc8a606ebba53496fc462092b76
Henrique Jensen <henriquejensen@hotmail.com>
2022-05-03 19:48:22 -0300

    ajuste do código inicial

--- src/test/exercise-one.test.js
+++ src/test/exercise-one.test.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 // arquivo App.test.js pode servir de exemplo
-describe('Teste da aplicação, testando o botão e sua funcionalidade', () => {
-  test('Verificando se o botão está na tela e se o ele contém o texto "Adicionar"', () => {
-    const { getByText } = render(<App />);
+describe('Testando a aplicação, testando botão, e sua funcionalidade', () => {
+  test('Verifica se o botão está na tela com o texto "Adicionar"', () => {
+    render(<App />);
   });
 
-  test('Ao clicar no botão, é necessário adicionar o que o usuário digitou à lista', () => {
+  test('Verifica se ao ser clicado é adicionado o que o usuário digitou', () => {
     // Use o userEvent, para simular a digitação do usuário e o clique.
-    const { getByLabelText, queryByText } = render(<App />);
+    render(<App />);
   });
 });
