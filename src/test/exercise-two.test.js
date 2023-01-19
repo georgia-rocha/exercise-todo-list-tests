c21867707efda1fc76e3ac946eee51ed6cf5123e
Georgia <georgiab543@gmail.com>
2023-01-19 00:57:43 -0300

    requisito 2 finalizado

--- src/test/exercise-two.test.js
+++ src/test/exercise-two.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import Item from '../Item';
 
@@ -8,11 +8,24 @@ describe('Teste do campo de input', () => {
   test('Testando a adição de vários itens a aplicação', () => {
     const listTodo = ['Realizar CR', 'Ler Post no Medium', 'Beber água']; // Use esse array como base para realizar os testes.
     render(<App />); // Caso precise de uma nova query adicione no object destructuring
+    const inputTarefa = screen.getByLabelText('Tarefa:');
+    const buttonAdicionar = screen.getByText('Adicionar');
+
+    listTodo.forEach((task) => {
+      userEvent.type(inputTarefa, task);
+      userEvent.click(buttonAdicionar);
+    });
+
+    listTodo.forEach((task) => {
+      const currentTask = screen.getByText(task);
+      expect(currentTask).toBeInTheDocument();
+    });
   });
 });
 
 describe('Teste do componente Item', () => {
   test('Ao receber uma string como prop ela precisa aparecer na tela', () => {
-
+    render(<Item content="string" />);
+    expect(screen.getByText('string')).toBeInTheDocument();
   });
 });
